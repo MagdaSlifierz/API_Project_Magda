fd2ec83a5e1fa83d7250a652089466f738b6469d
Magdalena Slifierz <m.slifierz@gmail.com>
2023-02-26 21:55:49 -0800

    projectdone

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import APIService from '../APIService'
+import APIService from '../APIService';
+import { useCookies } from 'react-cookie';
+
 function ArticleList(props) {
 
+  const [token] = useCookies(['mytoken'])
   const editBtn = (article) => {
     props.editBtn(article)
   }
 
   const deleteBtn = (article) => {
-    APIService.DeleteArticle(article.id)
+    APIService.DeleteArticle(article.id, token['mytoken'])
     .then (() => props.deleteBtn(article))
     .catch(error => console.log(error))
   }
   return (
     <div>
-      
       {props.articles && props.articles.map(article => {
         return (
             <div key = {article.id}>
@@ -24,7 +26,7 @@ function ArticleList(props) {
                   <div className='col-md-1'>
                   <button className='btn btn-primary' onClick = {() => editBtn(article)}>Update</button>
                   </div>
-                  <div className='col'>
+                  <div className='col-md-1'>
                   <button className='btn btn-danger' onClick = {() => deleteBtn(article)}>Delete</button>
                   </div>
                 </div>
